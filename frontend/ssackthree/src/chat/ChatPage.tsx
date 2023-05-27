e641a227056eaf2bc911e46d54f41384ea549f46
mingold73 <119676221+mingold73@users.noreply.github.com>
2023-05-27 17:50:31 +0900

    Add files via upload
    채팅창 UI

--- frontend/ssackthree/src/chat/ChatPage.tsx
+++ frontend/ssackthree/src/chat/ChatPage.tsx
@@ -1,11 +1,166 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Text} from '../components/text';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, TextInput, Image, StyleSheet,ImageSourcePropType } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
-export default function ChatPage(): JSX.Element {
+interface ChatItem {
+  id: number;
+  profileImage: any;
+  name: string;
+  role: string;
+  message: string;
+  unreadCount: number;
+}
+
+const Chatpage: React.FC = () => {
+  const [chatData, setChatData] = useState<ChatItem[]>([
+    {
+      id: 1,
+      profileImage: 'https://via.placeholder.com/150',
+      name: '가게이름',
+      role: '점주',
+      message: '마감시간이 10분 ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ연장되어 채팅드립니다',
+      unreadCount: 2,
+    },
+    {
+      id: 2,
+      profileImage: 'https://via.placeholder.com/150',
+      name: '눈송와플집',
+      role: '점주',
+      message: '알겠습니다.',
+      unreadCount: 0,
+    },
+
+  ]);
+
+  const navigation = useNavigation();
+
+  const renderChatItem = ({ item }: { item: ChatItem }) => {
+    const { profileImage, name, role, message, unreadCount } = item;
+
+    return (
+      <TouchableOpacity style={styles.chatItemContainer} onPress={() => navigation.navigate('ChatScreen', { name,role })}>
+        <Image source={{ uri: profileImage }} style={styles.profileImage} />
+        <View style={styles.chatItemContent}>
+          <View style={styles.chatItemHeader}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.role}>{role}</Text>
+          </View>
+          <View style={styles.chatItemBody}>
+            <Text numberOfLines={1} ellipsizeMode="tail" style={styles.message}>{message}</Text>
+            {unreadCount > 0 && <View style={styles.unreadBadge}><Text style={styles.unreadCount}>{unreadCount}</Text></View>}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  
+
   return (
-    <View>
-      <Text>채팅 페이지</Text>
+    <View style={styles.container}>
+      <View style={styles.appBar}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <IonIcon name="chevron-back-outline" size={30} color="#616161" />
+        </TouchableOpacity>
+        <Text style={styles.pageTitle}>채팅</Text>
+      </View>
+      <View style={styles.container2}>
+      <FlatList
+        data={chatData}
+        renderItem={renderChatItem}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding:1,
+    backgroundColor:'white'
+  },
+  container2: {
+    flex: 1,
+    padding:10,
+    backgroundColor:'white'
+  },
+  appBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 56,
+    paddingHorizontal: 10,
+  },
+  pageTitle: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
+    marginRight: 30,
+  },
+  chatItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginTop:5,
+  },
+  chatItemContent: {
+    marginLeft: 16,
+    flex: 1,
+  },
+  chatItemHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginRight:5,
+    color:'black'
+  },
+  role: {
+    fontSize: 13,
+    color:'black'
+  },
+  chatItemBody: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  message: {
+    flex: 1,
+    fontSize: 15,
+    marginRight:10,
+    color:'black'
+  },
+  unreadBadge: {
+    width: 30,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#94E048',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  unreadCount: {
+    fontSize: 12,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'lightgray',
+    marginVertical: 8,
+  },
+});
+
+export default Chatpage;
---
+++ frontend/ssackthree/src/chat/ChatScreen.tsx
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+const ChatScreen = () => {
+    const [messages, setMessages] = useState<Array<{ text: string; sender: string; time: number; date: string }>>([]);
+    const [inputText, setInputText] = useState('');
+
+    const navigation = useNavigation();
+    const route = useRoute();
+    const { name, role } = route.params;
+
+    const handleSendMessage = () => {
+        if (inputText.trim() !== '') {
+            const currentTime = new Date().getTime();
+            const currentDate = new Date().toISOString().split('T')[0];
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { text: inputText, sender: 'me', time: currentTime, date: currentDate },
+            ]);
+            setInputText('');
+        }
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const receivedMessage = '상대방이 보낸 메시지';
+            const currentTime = new Date().getTime();
+            const currentDate = new Date().toISOString().split('T')[0];
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                { text: receivedMessage, sender: 'other', time: currentTime, date: currentDate },
+            ]);
+        }, 5000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    const formatTime = (timestamp: number) => {
+        const date = new Date(timestamp);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+        return formattedTime;
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.appBar}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <IonIcon name="chevron-back-outline" size={30} color="#616161" />
+                </TouchableOpacity>
+                <View style={styles.pageContainer}>
+                    <View style={styles.pageTitleContainer}>
+                        <Text style={styles.pageTitle}>{name}</Text>
+                        <Text style={styles.pageTitle2}>{role}</Text>
+                    </View>
+                </View>
+            </View>
+            <ScrollView contentContainerStyle={styles.messagesContainer}>
+                {messages.map((message, index) => {
+                    const previousMessage = messages[index - 1];
+                    const currentDate = message.date;
+                    const previousDate = previousMessage?.date;
+
+                    const shouldDisplayDate = currentDate !== previousDate;
+
+                    return (
+                        <React.Fragment key={index}>
+                            {shouldDisplayDate && <Text style={styles.dateSeparator}>{currentDate}</Text>}
+                            <View
+                                style={[
+                                    styles.messageItem,
+                                    message.sender === 'me' ? styles.myMessage : styles.otherMessage,
+                                ]}
+                            >
+                                <Text style={styles.messageText}>{message.text}</Text>
+                                <Text style={styles.messageTime}>{formatTime(message.time)}</Text>
+                            </View>
+                        </React.Fragment>
+                    );
+                })}
+            </ScrollView>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    value={inputText}
+                    onChangeText={setInputText}
+                    placeholder="메시지를 입력하세요"
+                    placeholderTextColor="#FFFFFF"
+                />
+                <TouchableOpacity onPress={handleSendMessage}>
+                    <IonIcon name="paper-plane-outline" size={30} color="#231F20" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    appBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 56,
+        paddingHorizontal: 10,
+    },
+    pageContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 20,
+    },
+    pageTitleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    pageTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    pageTitle2: {
+        fontSize: 14,
+        color: 'black',
+        marginLeft: 5,
+    },
+    messagesContainer: {
+        flexGrow: 1,
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+    },
+    messageItem: {
+        backgroundColor: '#ECECEC',
+        borderRadius: 8,
+        padding: 12,
+        marginBottom: 8,
+    },
+    messageText: {
+        fontSize: 16,
+        color: 'black',
+    },
+    messageTime: {
+        fontSize: 12,
+        color: '#808080',
+        marginTop: 4,
+        alignSelf: 'flex-end',
+    },
+    myMessage: {
+        alignSelf: 'flex-end',
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: '#398908',
+    },
+    otherMessage: {
+        alignSelf: 'flex-start',
+        backgroundColor: '#FFFFFF',
+        borderWidth: 1,
+        borderColor: '#94E048',
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderTopColor: '#DDDDDD',
+    },
+    input: {
+        flex: 1,
+        height: 40,
+        backgroundColor: '#94E048',
+        borderRadius: 10,
+        paddingHorizontal: 12,
+        marginRight: 8,
+        color: 'white',
+    },
+    dateSeparator: {
+        textAlign: 'center',
+        marginTop: 16,
+        marginBottom: 8,
+        color: '#808080',
+        fontSize: 12,
+    },
+});
+
+export default ChatScreen;
